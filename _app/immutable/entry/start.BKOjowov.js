69f28f479d3c1211423b586655a3e9b17c74c47e
Marko Tiitto <marko.tiitto@hotmail.fi>
2024-04-20 17:39:08 +0300

    Updates

--- _app/immutable/entry/start.BKOjowov.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.CbfeP0Na.js";export{t as start};
---
+++ _app/immutable/nodes/2.CCeMvfBP.js
@@ -0,0 +1,14 @@
+import{s as h,n as i,g as v,o as u}from"../chunks/scheduler.CWzULmeO.js";import{S as f,i as g,e as d,s as j,h as T,c as m,d as n,a as x,g as M,b as l,f as $,j as p}from"../chunks/index.BumgRyWf.js";import{b as o}from"../chunks/paths.BS7V_aOB.js";function b(c){let e,_,t,r=`<h1 class="svelte-1in9dlj">This is home</h1> <div class="svelte-1in9dlj"><pre class="svelte-1in9dlj">            ######
+        #### ####
+        ## #######
+        ##### ###
+         _##
+_______|_|____
+//            \\\\
+//  ___     ___ \\\\
+//  |_|_|   |_|_| \\\\
+//___|_|_|___|_|_|__\\\\
+|    _____      _    |
+|   |_____|    |.|   |
+|___|_____|____|_|___|
+	</pre></div> <div class="main-page-browser-api-list svelte-1in9dlj"><a href="${o+"/useragent"}" class="svelte-1in9dlj">UserAgent</a> <a href="${o+"/geolocation"}" class="svelte-1in9dlj">Location</a> <a href="${o+"/mediadevices"}" class="svelte-1in9dlj">MediaDevices</a></div>`;return{c(){e=d("meta"),_=j(),t=d("section"),t.innerHTML=r,this.h()},l(s){const a=T("svelte-t32ptj",document.head);e=m(a,"META",{name:!0,content:!0}),a.forEach(n),_=x(s),t=m(s,"SECTION",{class:!0,"data-svelte-h":!0}),M(t)!=="svelte-fggyf0"&&(t.innerHTML=r),this.h()},h(){document.title="Home",l(e,"name","description"),l(e,"content","Svelte demo app"),l(t,"class","main-page-section svelte-1in9dlj")},m(s,a){$(document.head,e),p(s,_,a),p(s,t,a)},p:i,i,o:i,d(s){s&&(n(_),n(t)),n(e)}}}const C="Home";function H(c){const e=v("updateTitle");return u(()=>{e(C)}),[]}class L extends f{constructor(e){super(),g(this,e,H,b,h,{})}}export{L as component};
